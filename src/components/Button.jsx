b91c63166557422435fdf8ae9b6cebe2842b8157
TwickE <118013263+TwickE@users.noreply.github.com>
2024-10-24 14:28:02 +0100

    Finished the Subscribe  nad Footer sections

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ label, iconURL, backgroundColor, textColor, borderColor }) => {
+const Button = ({ label, iconURL, backgroundColor, textColor, borderColor, fullWidth }) => {
   return (
-    <button className={`${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'} flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full`}>
+    <button className={`${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : 'bg-coral-red text-white border-coral-red'} ${fullWidth && 'w-full'} flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full`}>
         {label}
         {
             iconURL && <img src={iconURL} alt="arrow right icon" className="ml-2 rounded-full w-5 h-5" />

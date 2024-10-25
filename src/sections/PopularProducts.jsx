7d69503fe4fb86699d2e90454f638763e9f341f5
TwickE <118013263+TwickE@users.noreply.github.com>
2024-10-25 16:22:16 +0100

    Added a key to list items

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -11,8 +11,8 @@ const PopularProducts = () => {
             </div>
             <div className='mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14 w-fit my-auto'>
                 {products.map((product, index) => (
-                    <ScrollAnimation animateIn='fadeInDownBig' delay={index === 0 ? 0 : index*100}>
-                        <PopularProductCard key={index} {...product} />
+                    <ScrollAnimation key={index} animateIn='fadeInDownBig' delay={index === 0 ? 0 : index*100}>
+                        <PopularProductCard {...product} />
                     </ScrollAnimation>
                 ))}
             </div>
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -6,8 +6,8 @@ const Services = () => {
     return (
         <section className="max-container flex flex-wrap justify-center gap-9 padding-x py-10">
             {services.map((service, index) => (
-                <ScrollAnimation animateIn='fadeInDownBig' delay={index === 0 ? 0 : index*100}>
-                    <ServiceCard key={index} {...service} />
+                <ScrollAnimation key={index} animateIn='fadeInDownBig' delay={index === 0 ? 0 : index*100}>
+                    <ServiceCard {...service} />
                 </ScrollAnimation>
             ))}
         </section>
